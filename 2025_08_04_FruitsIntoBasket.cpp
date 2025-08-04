6892f09ae397627e609b55871f74ba88d27e0ede
khadijaAli12 <khadijaali5858@gmail.com>
2025-08-05 03:51:12 +0500

    'FruitsIntoBasket'

---
+++ 2025_08_04_FruitsIntoBasket.cpp
@@ -0,0 +1,35 @@
+// LeetCode POTD - 2025/08/04
+// Problem: Fruit Into Baskets
+// Link: https://leetcode.com/problems/fruit-into-baskets/
+// Tags: Sliding Window, Hash Map
+// Approach: Maintain a sliding window with at most 2 types of fruits.
+
+class Solution {
+    public:
+        int totalFruit(vector<int>& fruits) {
+            int n = fruits.size();
+            unordered_map<int, int> mp;
+            int i = 0, j = 0;
+            int count = 0;
+    
+            while (j < n) {
+                mp[fruits[j]]++; // Add fruit to the basket
+    
+                if (mp.size() <= 2) {
+                    count = max(count, j - i + 1); // Update max if valid
+                } else {
+                    // Shrink window from left
+                    mp[fruits[i]]--;
+                    if (mp[fruits[i]] == 0) {
+                        mp.erase(fruits[i]);
+                    }
+                    i++;
+                }
+    
+                j++;
+            }
+    
+            return count;
+        }
+    };
+    
